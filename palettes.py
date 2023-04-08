de033368242baf22f5425df8d7eb294b8a4d78a5
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2023-04-08 11:19:22 +0300

    added a moving ball

--- palettes.py
+++ palettes.py
@@ -3,4 +3,4 @@ import pygame
 BLACK = pygame.Color(0, 0, 0)
 GRAY = pygame.Color(96, 96, 96)
 WHITE = pygame.Color(255, 255, 255)
-
+YELLOW = pygame.Color(255, 255, 0)
--- pypong.py
+++ pypong.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python3
 from picasso import PicassoEngine
-from palettes import BLACK, WHITE
-from collections import namedtuple
+from palettes import BLACK, WHITE, YELLOW, GRAY
+from random import randint, choice
+from dataclasses import dataclass
 
 import pygame
 
 
-Point = namedtuple("Point", ["x", "y"])
-
 WINDOW_SIZE = 1024, 768
 WIDTH, HEIGHT = WINDOW_SIZE
 MIDDLE = WIDTH // 2
-THICKNESS = 8
+WALLMARK_THICKNESS = 5
 
 STICK_WIDTH = 20
 STICK_HEIGHT = 150
 STICK_VELOCITY = 13
 
+BALL_THICKNESS = 13
+
 LEFT_LANE = 80
 RIGHT_LANE = WIDTH - LEFT_LANE - STICK_WIDTH
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+    @property
+    def pos(self):
+        return self.x, self.y
 
 
 class PyPongGame(PicassoEngine):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.frame_counter = 0
-    
+
+        # players position
         self.left_player = Point(LEFT_LANE, 100)
         self.right_player = Point(RIGHT_LANE, 100)
+
+        # ball info
+        self.ball = Point(MIDDLE, HEIGHT // 2)
+        self.velocity = Point(
+            choice((-1, 1)) * randint(5, 15), choice((-1, 1)) * randint(5, 15)
+        )
 
         self.left_up = False
         self.left_down = False
@@ -39,10 +57,12 @@ class PyPongGame(PicassoEngine):
 
     def on_paint(self):
         self.update_player_moves()
-        
+        self.update_ball_move()
+
         self.draw_field()
         self.draw_stick(self.left_player)
         self.draw_stick(self.right_player)
+        self.draw_ball()
 
         self.frame_counter += 1
         if self.frame_counter % 1000 == 0:
@@ -59,13 +79,13 @@ class PyPongGame(PicassoEngine):
             bye = pygame.event.Event(pygame.QUIT)
             pygame.event.post(bye)
             return
-        
+
         pressed = pygame.key.get_pressed()
         self.left_up = pressed[pygame.K_w]
         self.left_down = pressed[pygame.K_s]
         self.right_up = pressed[pygame.K_UP]
         self.right_down = pressed[pygame.K_DOWN]
-    
+
     def update_player_moves(self):
         if self.left_up and self.left_player.y > 0:
             new_y = self.left_player.y - STICK_VELOCITY
@@ -81,16 +101,47 @@ class PyPongGame(PicassoEngine):
             new_y = self.right_player.y + STICK_VELOCITY
             self.right_player = Point(self.right_player.x, new_y)
 
+    def update_ball_move(self):
+        new_pos = Point(
+            self.ball.x + self.velocity.x,
+            self.ball.y + self.velocity.y,
+        )
+
+        if new_pos.x > (WIDTH - BALL_THICKNESS):
+            over_x = new_pos.x - (WIDTH - BALL_THICKNESS)
+            new_pos.x -= over_x
+            self.velocity.x = -self.velocity.x
+
+        if new_pos.x < (0 + BALL_THICKNESS):
+            over_x = new_pos.x - (0 + BALL_THICKNESS)
+            new_pos.x -= over_x
+            self.velocity.x = -self.velocity.x
+
+        if new_pos.y > (HEIGHT - BALL_THICKNESS):
+            over_y = new_pos.y - (HEIGHT - BALL_THICKNESS)
+            new_pos.y -= over_y
+            self.velocity.y = -self.velocity.y
+
+        if new_pos.y < (0 + BALL_THICKNESS):
+            over_y = new_pos.y - (0 + BALL_THICKNESS)
+            new_pos.y -= over_y
+            self.velocity.y = -self.velocity.y
+
+        self.ball = new_pos
+
     def draw_field(self):
         self.screen.fill(BLACK)
         N = 20
         for i in range(N):
             center = MIDDLE, (i * HEIGHT) // N
-            pygame.draw.circle(self.screen, WHITE, center, THICKNESS)
+            pygame.draw.circle(self.screen, GRAY, center, WALLMARK_THICKNESS)
 
     def draw_stick(self, position):
         r = pygame.Rect(position.x, position.y, STICK_WIDTH, STICK_HEIGHT)
         pygame.draw.rect(self.screen, WHITE, r)
+
+    def draw_ball(self):
+        pygame.draw.circle(self.screen, YELLOW, self.ball.pos, BALL_THICKNESS)
 
 
 def main():
